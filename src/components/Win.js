9cf7ad4df5540d004a87e2f2d22b41ab76cec6a9
Vishesh Pandey <87028246+VisheshPandey@users.noreply.github.com>
2022-12-28 01:47:54 +0000

    updated form

--- src/components/Win.js
+++ src/components/Win.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { useState } from 'react'
 
 function Win() {
+    const [terms, setTerms] = useState(false)
+
     return (
         <div className="container">
             <div className="row">
                 <div className="col text-center">
-                    <h1>Your Answer was correct</h1>
+                    <h1 className='text-success'>Congratulations! <br/>Your Answer was correct</h1>
                 </div>
             </div>
             <div className="row">
@@ -15,7 +18,7 @@ function Win() {
             </div>
             <div className="row">
                 <div className="col text-center">
-                    <p className="text-success fs-2">To Claim Your Prize please provide us your few details</p>
+                    <p className="text-warning fs-2">To Claim Your Prize please provide us your few details</p>
                 </div>
             </div>
             <div className="row">
@@ -35,18 +38,18 @@ function Win() {
                         </div>
                         <div className="col-12">
                             <label htmlFor="inputAddress" className="form-label">
-                                Address
+                                PAN Number
                             </label>
                             <input
                                 type="text"
                                 className="form-control"
                                 id="inputAddress"
-                                placeholder="1234 Main St"
+                                placeholder="GAW*******"
                             />
                         </div>
                         <div className="col-12">
                             <label htmlFor="inputAddress2" className="form-label">
-                                Address 2
+                                Address 
                             </label>
                             <input
                                 type="text"
@@ -57,19 +60,16 @@ function Win() {
                         </div>
                         <div className="col-md-6">
                             <label htmlFor="inputCity" className="form-label">
-                                City
+                                State - City
                             </label>
                             <input type="text" className="form-control" id="inputCity" />
                         </div>
                         <div className="col-md-4">
-                            <label htmlFor="inputState" className="form-label">
+                            <label htmlFor="inputCountry" className="form-label">
                                 Country
                             </label>
-                            <select id="inputState" className="form-select">
-                                <option selected="">Choose...</option>
-                                <option>India</option>
-                                <option>Others</option>
-                            </select>
+                            <input type="text" className="form-control" id="inputCountry" />
+
                         </div>
                         <div className="col-md-2">
                             <label htmlFor="inputZip" className="form-label">
@@ -79,14 +79,14 @@ function Win() {
                         </div>
                         <div className="col-12">
                             <div className="form-check">
-                                <input className="form-check-input" type="checkbox" id="gridCheck" />
+                                <input onClick={()=>{if(terms){setTerms(false)}else{setTerms(true)}}} className="form-check-input" type="checkbox" id="gridCheck" />
                                 <label className="form-check-label" htmlFor="gridCheck">
                                     I agree to all terms and conditions
                                 </label>
                             </div>
                         </div>
                         <div className="col-12">
-                            <button type="submit" className="btn btn-primary">
+                            <button type="submit" className="btn btn-primary" disabled={!terms}>
                                 Submit
                             </button>
                         </div>
